2ea166a2edfc4c1ce0126f164c4aabdd54ed1cd6
Abdullah Ali <abdullah.ali@neomediaworld.com>
2019-02-06 14:41:21 -0500

    dependencies

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.send('OK');
+});
+
+io.on('connection', function(socket) {
+	console.log('A user connected');
+ 
+	//Send a message after a timeout of 4seconds
+	setTimeout(function() {
+	   socket.send('Sent a message 4seconds after connection!');
+	}, 4000);
+ 
+	socket.on('disconnect', function () {
+	   console.log('A user disconnected');
+	});
+ });
+
+// Setup
+http.listen(3000,function() {
+	console.log('listening on *:3000');
+});
